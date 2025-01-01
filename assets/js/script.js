543558204151f0ea22f9895c9995871565af7873
somenath singh <144797336+itsnarutouzumaki@users.noreply.github.com>
2025-01-01 21:23:46 +0530

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Somenath Singh";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
